4410e64d07674499e4cebfdaa46879cb832c8b5f
Elshater <elshater.ehab@gmail.com>
2023-08-01 22:13:02 +0300

    adding template

--- main_app/views.py
+++ main_app/views.py
@@ -4,6 +4,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+name = 'Gold'
+value = '1000'
+context = {'treausre_name': name,
+           'treausre_value': value}
+
 
 def index(request):
-    return HttpResponse("<h1>Hello</h1>")
+    return render(request, 'index.html', context)
